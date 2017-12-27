f5ca99fae88d749402c7ee539f10aed7a9d5d7f6
umjammer <umjammer@gmail.com>
2017-12-27 13:43:21 +0900

    tidy up

--- src/main/java/SequenceFrame.java
+++ src/main/java/SequenceFrame.java
@@ -26,7 +26,7 @@ import javax.swing.JScrollPane;
 /**
  * SequenceFrame.
  *
- * @author <a href="mailto:vavivavi@yahoo.co.jp">Naohide Sano</a> (nsano)
+ * @author <a href="mailto:umjammer@gmail.com">Naohide Sano</a> (nsano)
  * @version 0.00 070618 nsano initial version <br>
  */
 public class SequenceFrame {
--- src/main/java/SequenceTable.java
+++ src/main/java/SequenceTable.java
@@ -36,7 +36,7 @@ import vavi.util.StringUtil;
 /**
  * TableModel for MFi Sequence
  *
- * @author <a href="mailto:vavivavi@yahoo.co.jp">Naohide Sano</a> (nsano)
+ * @author <a href="mailto:umjammer@gmail.com">Naohide Sano</a> (nsano)
  * @version 0.00 030920 nsano initial version <br>
  */
 public class SequenceTable extends JTable {
